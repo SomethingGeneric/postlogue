b78d093ca2ee47545899b432302a45fe2aa37a7c
Matt C <matthew.compton@mymail.champlain.edu>
2022-10-06 00:12:08 -0400

    good lord man

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,25 @@
 #[macro_use] extern crate rocket;
 
-use std::borrow::Cow;
-
-use rocket::State;
-use rocket::tokio::sync::Mutex;
-use rocket::serde::json::{Json, Value, json};
-use rocket::serde::{Serialize, Deserialize};
-
-#[derive(Serialize, Deserialize)]
-#[serde(crate = "rocket::serde")]
-struct Gitea<'r> {
-    rref: Option<str>,
-    action: Option<str>,
-    repository: Option<str>
-}
+use serde_json::{Result, Value};
 
 #[get("/")]
 fn index() -> &'static str {
     "Go away"
 }
 
-#[post("/run", format="json", data="<webhook>")]
-fn run(webhook: Json<Gitea>) -> &'static str {
-    println!(webhook);
-    json!({"foo":"bar"})
+#[post("/run", format="application/json", data="<webhook>")]
+fn run(webhook: String) -> &'static str { 
+    let webdat: Value = serde_json::from_str(&webhook).unwrap();
+
+    // These fail if there is no such key, and I continue to fail
+    // in regards to figuring out how to check the available values
+    let rref = webdat.get("ref").unwrap();
+    let action = webdat.get("action").unwrap();
+
+    return "Foo";
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .mount("/", routes![index])
-        .mount("/run", routes![run])
+    rocket::build().mount("/", routes![index, run])
 }
